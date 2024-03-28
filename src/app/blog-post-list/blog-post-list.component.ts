2e8efbe721844c59618680bc4d6700740857c688
Your Nameomar <omargamal.2tecno@gmail.com>
2024-03-29 00:38:52 +0200

    handling not found

--- src/app/blog-post-list/blog-post-list.component.ts
+++ src/app/blog-post-list/blog-post-list.component.ts
@@ -2,11 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { PostService } from '../services/post.service';
 import { BlogPostItemComponent } from '../blog-post-item/blog-post-item.component';
-import {Posts} from "../posts"
+import { Posts } from '../posts';
 import { SearchPipe } from '../pipes/search.pipe';
 import { FormsModule } from '@angular/forms';
-
-
 
 @Component({
   selector: 'app-blog-post-list',
@@ -17,18 +15,21 @@ import { FormsModule } from '@angular/forms';
 })
 export class BlogPostListComponent {
   getPosts: Posts[] = [];
-  res:Posts[] = []
-  increment:number = 10
-  term:string = ''
+  res: Posts[] = [];
+  increment: number = 10;
+  term: string = '';
+  loading: boolean = false;
   constructor(private _PostService: PostService) {}
   ngOnInit() {
+    this.loading = true;
     this._PostService.getAllPosts().subscribe((res) => {
       this.getPosts = res.slice(0, 9);
-      this.res = res
+      this.loading = false;
+      this.res = res;
     });
   }
-  getArticals(){
-    this.increment +=10
-    this.getPosts = this.res.slice(0, this.increment) 
+  loadMore() {
+    this.increment += 10;
+    this.getPosts = this.res.slice(0, this.increment);
   }
 }
